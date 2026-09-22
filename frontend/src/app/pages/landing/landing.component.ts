4598074e1a0729d452aa16a9b9351a09620425b2
Samuel O <samuelonwukeme@gmail.com>
2026-09-22 13:14:30 +0000

    Integrating Spartan Components

--- frontend/src/app/pages/landing/landing.component.ts
+++ frontend/src/app/pages/landing/landing.component.ts
@@ -1,13 +1,13 @@
 import { Component, computed, signal } from '@angular/core';
 import {Chart } from '../chart/chart';
 import { HlmNavigationMenuImports } from '@spartan-ng/helm/navigation-menu';
-
+import {HlmTableImports } from '@spartan-ng/helm/table';
 @Component({
   selector: 'app-landing',
   standalone: true,
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css',
-  imports: [Chart, HlmNavigationMenuImports],
+  imports: [Chart, HlmNavigationMenuImports, HlmTableImports],
 })
 export class LandingComponent {
   private readonly currencyFormatter = new Intl.NumberFormat('en-US', {
