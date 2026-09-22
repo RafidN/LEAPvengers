6c1367778d8f21d893078107727f6ca9556f0394
Samuel O <samuelonwukeme@gmail.com>
2026-09-22 21:25:09 +0000

    Fixed Routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { SignInComponent } from './pages/sign-in/sign-in.component';
-import { RegisterComponent } from './pages/register/register.component';
+import { SignInComponent } from './pages/authentication/sign-in/sign-in.component';
+import { RegisterComponent } from './pages/authentication/register/register.component';
 import { LandingComponent } from './pages/landing/landing.component';
 import { ForgotPasswordComponent } from './pages/authentication/forgot-password/forgot-password.component';
 
